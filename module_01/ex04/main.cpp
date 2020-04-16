db0ee2166ad8a341785aab701bf224f1ba047bee
Jan Eudald Vaquer <janeudaldvaquer@Jans-MacBook-Pro.local>
2020-04-16 11:33:57 +0200

    ex04 (ez)

---
+++ module_01/ex04/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/16 11:18:45 by jvaquer           #+#    #+#             */
+/*   Updated: 2020/04/16 11:21:41 by jvaquer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int		main()
+{
+	std::string s = "HI THIS IS BRAIN";
+	std::string	*ptr = &s;
+	std::string	&ref = s;
+	
+	std::cout << *ptr << std::endl;
+	std::cout << ref << std::endl;
+}
