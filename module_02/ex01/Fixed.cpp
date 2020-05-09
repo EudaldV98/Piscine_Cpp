bfaab0850af0f71185dc9de036e52fc347f5242d
Jan Eudald Vaquer <janeudaldvaquer@Jans-MacBook-Pro.local>
2020-05-09 11:02:09 +0200

    done module02 TODO ALL BONUS

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -6,12 +6,13 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 21:53:41 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/05/08 23:55:17 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/05/09 10:59:03 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed(): raw(0)
 {
@@ -21,7 +22,17 @@ Fixed::Fixed(): raw(0)
 
 Fixed::Fixed(const Fixed &c): raw(c.raw)
 {
-	std::cout << "Copy constructor called." << std::cout;
+	std::cout << "Copy constructor called." << std::endl;
+}
+
+Fixed::Fixed(const int f): raw(f << fracBits)
+{
+	std::cout << "Int constructir called." << std::endl;
+}
+
+Fixed::Fixed(const float f): raw(roundf(f * power))
+{
+	std::cout << "Float constructor called." << std::endl;
 }
 
 int		Fixed::getRawBits() const
@@ -45,7 +56,23 @@ void	Fixed::setRawBits(const int r)
 	raw = r;
 }
 
+float	Fixed::toFloat(void) const
+{
+	return float(raw) / power;
+}
+
+int		Fixed::toInt(void) const
+{
+	// >> FOR SHIFTING
+	return raw >> fracBits;
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called." << std::endl;
+}
+
+std::ostream	&operator<<(std::ostream &o, const	Fixed &f)
+{
+	return o << f.toFloat();
 }
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -6,26 +6,37 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 21:53:21 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/05/08 23:55:26 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/05/09 10:45:34 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <ostream>
+
 class Fixed
 {
 	private:
 		int					raw;
 		static const int	fracBits = 8;
+		static const int	power = 256;
+		static const int	mask = 0xFF;
+
 	public:
 		Fixed();
 		Fixed(const Fixed &c);
-		Fixed &operator=(const Fixed &c);
+		Fixed(const int f);
+		Fixed(const float f);
+		Fixed 	&operator=(const Fixed &c);
 		~Fixed();
 
 		int		getRawBits() const;
 		void	setRawBits(const int r);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &f);
 
 #endif
