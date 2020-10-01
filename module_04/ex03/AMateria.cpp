a57eac46a786cfe5d887c5d9d45ee07098e1e2a1
Jan eudald VAQUER <jvaquer@e1r7p20.42.fr>
2020-10-01 15:10:14 +0200

    04-03

--- module_04/ex03/AMateria.cpp
+++ module_04/ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 16:31:33 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/09/26 19:00:12 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/10/01 15:10:00 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,15 @@ AMateria::AMateria(std::string const & type)
     _xp = 0;
 }
 
-AMateria::
+const std::string &AMateria::getType() const
+{
+    return _type;
+}
+
+unsigned int AMateria::getXP() const
+{
+    return  _xp;
+}
 
 AMateria::~AMateria()
 {
--- module_04/ex03/AMateria.hpp
+++ module_04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 16:31:23 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/09/26 18:25:49 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/10/01 14:53:51 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ class AMateria
 
 	public:
 	AMateria(std::string const & type);
-	~AMateria();
 	std::string const & getType() const; //Returns the materia type
 	unsigned int getXP() const; //Returns the Materia's XP
 	virtual AMateria* clone() const = 0;
+	~AMateria();
 	//virtual void use(ICharacter& target);
 };
